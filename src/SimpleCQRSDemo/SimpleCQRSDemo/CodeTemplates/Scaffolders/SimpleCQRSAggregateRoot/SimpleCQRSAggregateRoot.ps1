9a24c906d2fc9a6ca9339fbe1f666ef451fbf73d
Ferris Lucas <lucasf@digitalev.com>
2011-08-24 00:27:32 -0500

    further work on AggregateRoot template

--- src/SimpleCQRSDemo/SimpleCQRSDemo/CodeTemplates/Scaffolders/SimpleCQRSAggregateRoot/SimpleCQRSAggregateRoot.ps1
+++ src/SimpleCQRSDemo/SimpleCQRSDemo/CodeTemplates/Scaffolders/SimpleCQRSAggregateRoot/SimpleCQRSAggregateRoot.ps1
@@ -18,29 +18,20 @@ $namespace = (Get-Project $Project).Properties.Item("DefaultNamespace").Value
 
 Write-Host "Scaffolding AggregateRoot with $modelProjectType.Members.Count properties..."
 
+# Create Aggregate Root
 Add-ProjectItemViaTemplate "Domain\$Model" -Template SimpleCQRSAggregateRootTemplate `
     -Model @{ Namespace = $namespace; Name = $modelProjectType.Name } `
 	-SuccessMessage "complete {0}" `
 	-TemplateFolders $TemplateFolders -Project $Project -CodeLanguage $CodeLanguage -Force:$Force
 
+# Create Aggregate Root Created Event
+Add-ProjectItemViaTemplate ("Events\$Model" + "CreatedEvent") -Template SimpleCQRSAggregateRootCreatedEventTemplate `
+    -Model @{ Namespace = $namespace; Name = $modelProjectType.Name } `
+	-SuccessMessage "complete {0}" `
+	-TemplateFolders $TemplateFolders -Project $Project -CodeLanguage $CodeLanguage -Force:$Force
 
 foreach ($member in $modelProjectType.Members)
 {
     Write-Host "    Scaffolding property:" $member.Name
     #Add-ClassMemberViaTemplate -CodeClass $modelProjectType -Template "SimpleCQRSAggregateRootPropertyTemplate" -Model @{ PropertyName = $member.Name; Fullname = $member.FullName } -TemplateFolders $TemplateFolders
 }
-
-@'
-
-$outputPath = "ExampleOutput"  # The filename extension will be added based on the template's <#@ Output Extension="..." #> directive
-$namespace = (Get-Project $Project).Properties.Item("DefaultNamespace").Value
-
-
-write "asdfs"
-
-Add-ProjectItemViaTemplate $outputPath -Template SimpleCQRSAggregateRootTemplate3 `
-	-Model @{ Namespace = $namespace; ExampleValue = "Hello, world!" } `
-	-SuccessMessage "Added SimpleCQRSAggregateRoot output at {0}" `
-	-TemplateFolders $TemplateFolders -Project $Project -CodeLanguage $CodeLanguage -Force:$Force
-
-'@ > $null
